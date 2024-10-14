dfed4ea712108c57fc3bc68b44a013f49f04c25f
tabouri <lukas.tabouri@outlook.fr>
2024-10-14 09:22:43 +0200

    fix ptipython.sh

--- ptipython.sh
+++ ptipython.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+
 eval $(opam env)
 
+# Vérifier si un argument est fourni
+if [ -z "$1" ]; then
+  echo "Veuillez fournir un fichier en argument."
+  exit 1
+fi
+
+# Récupérer le nom du fichier
+fichier="$1"
+
+
+
+
+# Interpréter le fichier
 cd Interpreter_ptipython/base_runtime ;
 dune build ;
-./ptipython.exe ../base_json/file.py
+cd ../..;
+cp Interpreter_ptipython/base_runtime/ptipython.exe .
+./ptipython.exe $fichier
