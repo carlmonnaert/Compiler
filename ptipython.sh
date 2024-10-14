ec0f52699315e1e3eb0ee17586da20c5ca7f91b5
lukas.tabouri <lukas.tabouri@telecom-paris.fr>
2024-10-14 15:01:18 +0200

    bis

--- ptipython.sh
+++ ptipython.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-eval $(opam env)
+# eval $(opam env)
 
 
 
@@ -13,5 +13,5 @@ cd ../.. ;
 cp Interpreter_ptipython/base_runtime/ptipython.exe .
 cp Interpreter_ptipython/base_json/ptipython2json.exe .
 ./ptipython2json.exe file.py
-jq . file.json > file2.json
+# jq . file.json > file2.json
 ./ptipython.exe file.py
