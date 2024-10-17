b4c92257fb5abc73b9ed6de0368a5d3f5bb5a244
carl.monnaert <carl.monnaert@telecom-paris.fr>
2024-10-17 21:05:35 +0200

    fix

---
+++ file.py
@@ -0,0 +1,10 @@
+x = "Carl"
+
+while True:
+    x = "Carl"
+    print(x)
+    if "Carl" == x and x == "Carl" :
+        print("Yes")
+        print("No")
+        if True :
+            print("Pedro")
