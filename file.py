479700aa06f3350c9454409cae7e6c89459cf3da
carl.monnaert <carl.monnaert@telecom-paris.fr>
2024-10-18 21:25:09 +0200

    revert

--- file.py
+++ file.py
@@ -11,3 +11,21 @@ def myrange(n):
     return [0]
 
 print(myrange(5))
+
+
+for x in ["Je", 5 , False , None]:
+    if type(x) == "<class 'str'>" or type(x) == "<class 'int'>":
+        print(x)
+
+def complex(x,y,z):
+    print(x)
+    print(y)
+    print(z)
+    if x == 0 and y == 0 and z == 0:
+        return [x,y,z]
+    elif x > 0:
+        return complex(x-1,y,z)
+    else :
+        return complex(y,z,x)
+
+complex(2,2,2)
