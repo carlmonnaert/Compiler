cd0fc1ab89ebf2549058301c395e4e37538e2e03
lukas.tabouri <lukas.tabouri@telecom-paris.fr>
2024-11-10 12:47:09 +0100

    fix AND OR NOT

--- file.c
+++ file.c
@@ -1,9 +1,12 @@
+int i = 2;
+
+
 int main() {
-    int i = -10;
-    // La boucle s'exécute tant que i est inférieur à 0, i est incrémenté de 2 à chaque itération
-    while (i < 0) {
+    
+    while (i >= 0) {
         print_int(i);
-        i = i + 2;  // i est incrémenté de 2
+        i = i - 1;
+        
     }
     return 0;
 }
